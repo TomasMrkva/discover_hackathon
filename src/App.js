6e58d2c2e6dca7592a0ef1d39108388a125a1b0b
milesbroomfield <milesbroomfield@yahoo.com>
2021-08-13 12:00:18 +0100

    collage completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import Test from './Test'
 
-var pictures = ["https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all","https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all","https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all","https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all","https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all"]
+var pictures = ['https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg',"https://nypost.com/wp-content/uploads/sites/2/2021/08/rihanna-smells-really-good-323.jpg?quality=80&strip=all",'https://www.nme.com/wp-content/uploads/2020/05/GettyImages-1134174735.jpg']
 
 var numRows = 3
 var rows = []
@@ -22,16 +22,18 @@ function App() {
     rows[row].push(pictures[i]);
 
     //Increment or Reset
-    row = numRows == row ? 0 : row+1
+    row = numRows-1 == row ? 0 : row+1
   }
 
 
   return (
-    <div>
+    <div class = 'div'>
       {rows.map((row,i) => 
-        <span>
-        {row.map((image,k) => <img src={image} class='cover' key = {k}/>)}
-        </span>
+        <div>
+          {row.map((image,k) => 
+            <img src={image} class='cover' key = {k}/>
+          )}
+        </div>
       )}
     </div>
     // <Test/>
