f9bf1e6f48ad4b783ae441dfd030215221623c0a
Tomas Mrkva <22m.tomash@gmail.com>
2021-08-25 02:36:13 +0100

    navbar size increased v3

--- src/components/FabButton.js
+++ src/components/FabButton.js
@@ -6,7 +6,7 @@ import AddIcon from '@material-ui/icons/Add';
 const useStyles = makeStyles(theme => ({
     fab: {
       position: 'fixed',
-      bottom: theme.spacing(9.2),
+      bottom: theme.spacing(12),
       right: theme.spacing(2),
     },
   }));
